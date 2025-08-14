e835cfd9d64e0cc08c20417da77d8cd182840db7
EvgeniyKolmak <zenakolmak669@gmail.com>
2025-08-14 22:57:21 +0300

    hometask 22

---
+++ hometask22/src/main/java/com/kolmak/web/DateServlet.java
@@ -0,0 +1,21 @@
+package com.kolmak.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class DateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        Date date = new Date();
+        writer.println("Hello everyone! Today " + date);
+    }
+}
